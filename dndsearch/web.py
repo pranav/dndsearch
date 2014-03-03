05a9782ce455d8e14c9c3ea4a883404462a7ab3f
Pranav Gandhi <pgandhi@hubspot.com>
2014-03-02 20:21:07 -0500

    Whoops forgot web.py

---
+++ dndsearch/web.py
@@ -0,0 +1,19 @@
+import json
+from flask import Flask, abort, Response, render_template
+from sql import Page
+app = Flask(__name__)
+
+
+@app.route('/query/<query>', methods=['GET'])
+def simple_query(query):
+    return json.dumps([{'book': p[0], 'page': p[1]} for p in Page.search(query)])
+
+
+@app.route('/home')
+def render_home():
+    return render_template('home.html')
+    
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
