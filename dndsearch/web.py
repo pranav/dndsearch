c9403744c5999a5b58ab3ea91e23b96a3f2811bc
Pranav Gandhi <pgandhi@hubspot.com>
2014-03-02 20:56:36 -0500

    Changed route from /home to /

--- dndsearch/web.py
+++ dndsearch/web.py
@@ -9,10 +9,10 @@ def simple_query(query):
     return json.dumps([{'book': p[0], 'page': p[1]} for p in Page.search(query)])
 
 
-@app.route('/home')
+@app.route('/')
 def render_home():
     return render_template('home.html')
-    
+
 
 if __name__ == '__main__':
     app.debug = True
