ee2e159e9690abc6dd6a72d1b9dea337af7544fd
Pranav Gandhi <pgandhi@hubspot.com>
2014-12-21 02:11:14 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dndsearch',
+    version='1.0',
+    description='Search the DnD Books for something',
+    author='Pranav Gandhi',
+    author_email='me@pranav.io',
+    packages=find_packages(),
+    install_requires=[
+        'pyPdf',
+        'sqlalchemy',
+        'MySQL-python'
+    ]
+)
